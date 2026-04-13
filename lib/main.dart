2507894f5c86aff16d9c30c9aa741bb5852633bd
DanielFyn4 <danielanuar1207@gmail.com>
2026-04-13 23:07:16 +0800

    done today

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() { runApp
-  (MaterialApp
-  (home: Scaffold(
-  backgroundColor: Colors.yellow,
-  body: Center(
-    child: Text(
-        'WAN AHMAD DANIEL BIN WAN ANUAR',
-      style: TextStyle(fontSize: 50),
-  ),
-),
-  appBar:AppBar(
-    backgroundColor: Colors.blueGrey,
-    title: Center(child: Text('HELLO WORLD')),
-  ) ,
-),
-),
-);
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.purple,
+        body: Center(
+          child: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Text(
+              'WAN AHMAD DANIEL BIN WAN ANUAR',
+              style: TextStyle(
+                color: Colors.blue, fontWeight: FontWeight(1000)),),
+            alignment: Alignment.center,
+            width: 350,
+            height: 500,
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 10,
+            ),
+          ),
+        ),
+      ),
+      ),
+    );
+  }
 }
